121ca98fbd2020dbee03ff4ad0ed43f643f5ea94
buczek <buczekb6@gmail.com>
2020-10-21 19:33:24 +0200

    Updates

---
+++ precache-manifest.24ede2815e47f3f6cd347aa80fb00884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99252a7779574e4b61ffd6e533f3808a",
+    "url": "/game-bj/index.html"
+  },
+  {
+    "revision": "7111fdd6395c91b828b0",
+    "url": "/game-bj/static/css/main.4ab127e8.chunk.css"
+  },
+  {
+    "revision": "98b296c361f725c66d0f",
+    "url": "/game-bj/static/js/2.89a9d0d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-bj/static/js/2.89a9d0d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7111fdd6395c91b828b0",
+    "url": "/game-bj/static/js/main.e18b6a63.chunk.js"
+  },
+  {
+    "revision": "59f56f60b49a3856f130",
+    "url": "/game-bj/static/js/runtime-main.d8deb963.js"
+  }
+]);
